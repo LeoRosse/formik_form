7ba366da159e8ad5b7416079b9718106f2e106c1
Leonardo Rosseti <rosseti@extendi.it>
2019-10-03 11:55:46 +0200

    split app in separated component

--- src/index.js
+++ src/index.js
@@ -1,69 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { withFormik, Form, Field } from "formik";
+import { withFormik } from "formik";
 import * as yup from "yup";
-import TextField from "@material-ui/core/TextField";
 
-const App = ({ values, errors, touched }) => (
-  <Form>
-    <div>
-      <Field
-        name="email"
-        value={values.email}
-        render={({ field, form }) => {
-          return (
-            <TextField
-              id="email"
-              label="Email"
-              value={field.value}
-              name={field.name}
-              onChange={e => form.setFieldValue(field.name, e.target.value)}
-              margin="normal"
-              variant="outlined"
-              error={touched.email && errors.email}
-              helperText={touched.email && errors.email && `${errors.email}`}
-            />
-          );
-        }}
-      />
-    </div>
-    <div>
-      <Field
-        name="password"
-        value={values.password}
-        render={({ field, form }) => {
-          return (
-            <TextField
-              id="password"
-              label="Password"
-              value={field.value}
-              name={field.name}
-              onChange={e => form.setFieldValue(field.name, e.target.value)}
-              margin="normal"
-              type="password"
-              variant="outlined"
-              error={touched.password && errors.password}
-              helperText={
-                touched.password && errors.password && `${errors.password}`
-              }
-            />
-          );
-        }}
-      />
-      {touched.password && errors.password && <p>{errors.password}</p>}
-    </div>
-    <label>
-      <Field type="checkbox" name="newsletter" checked={values.newsletter} />
-      Join our newsletter
-    </label>
-    <Field component="select" name="plan">
-      <option value="free">Free</option>
-      <option value="premium">Premium</option>
-    </Field>
-    <button type="submit">Submit</button>
-  </Form>
-);
+//compoments
+import { App } from "./components/app";
 
 const FormikApp = withFormik({
   mapPropsToValues({ email, password, newsletter, plan }) {
